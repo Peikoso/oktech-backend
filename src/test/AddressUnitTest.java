100e8718532bb248f0a9a6a1c0a38adc716a1e06
João Martins <joaommn1998@gmail.com>
2025-09-25 22:41:08 -0300

    test: add unit tests for Address entity constructor and accessors

---
+++ src/test/AddressUnitTest.java
@@ -0,0 +1,40 @@
+package com.oktech.boasaude.entity;
+
+import com.oktech.boasaude.dto.AddressCreateRequestDto;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressUnitTest {
+
+    @Test
+    void constructorFromDto_setsFields() {
+        AddressCreateRequestDto dto = new AddressCreateRequestDto(
+            "Rua Exemplo", "Salvador", "BA", "Apto 1", "40110150"
+        );
+
+        Address address = new Address(dto);
+
+        assertEquals("Rua Exemplo", address.getStreet());
+        assertEquals("Salvador", address.getCity());
+        assertEquals("BA", address.getState());
+        assertEquals("Apto 1", address.getComplement());
+        assertEquals("40110150", address.getCep());
+    }
+
+    @Test
+    void settersAndGetters_work() {
+        Address a = new Address();
+        a.setStreet("Outra Rua");
+        a.setCity("Feira");
+        a.setState("BA");
+        a.setComplement("Casa");
+        a.setCep("44000000");
+
+        assertEquals("Outra Rua", a.getStreet());
+        assertEquals("Feira", a.getCity());
+        assertEquals("BA", a.getState());
+        assertEquals("Casa", a.getComplement());
+        assertEquals("44000000", a.getCep());
+    }
+}
